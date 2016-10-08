0a08910d299555ffb11cbef38d8b75f5d5f9d0a7
Martin Milota <milotov@seznam.cz>
2016-10-08 12:52:35 +0200

    Initial commit

---
+++ 1ExternalSort/1ExternalSort/ExternalSorter/ExternalSorter.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace _1ExternalSort.ExternalSorter
+{
+    public class ExternalSorter
+    {
+        private string m_filePath;
+
+        public ExternalSorter(string filePath)
+        {
+            Debug.Assert(!string.IsNullOrEmpty(filePath));
+            m_filePath = filePath;
+        }
+
+
+    }
+}
