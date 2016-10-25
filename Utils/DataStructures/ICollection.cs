8330ada63dd0b94324821e05db22c55e38dec3aa
Martin Milota <milotov@seznam.cz>
2016-10-25 16:43:22 +0200

    Added the splay tree solution

---
+++ Utils/DataStructures/ICollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.DataStructures
+{
+    public interface ICollection<T>
+    {
+        void Insert(T item);
+        void Delete(T item);
+    }
+}
