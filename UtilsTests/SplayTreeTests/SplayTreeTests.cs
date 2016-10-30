d1efcf4e96a8818637fbed579fcf9c44bf77a28e
Martin Milota <milotov@seznam.cz>
2016-10-30 11:18:32 +0100

    * Added the Utils unit tests

---
+++ UtilsTests/SplayTreeTests/SplayTreeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilsTests
+{
+    [TestClass]
+    public class SplayTreeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
