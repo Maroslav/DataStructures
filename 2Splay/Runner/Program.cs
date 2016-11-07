13807933f29d93814bf6382ba29369ab4546563f
Martin Milota <milotov@seznam.cz>
2016-11-07 23:06:02 +0100

    @ Added post-build command for Runner

--- 2Splay/Runner/Program.cs
+++ 2Splay/Runner/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UtilsTests.SplayTree;
 
 namespace Runner
 {
@@ -10,6 +11,9 @@ namespace Runner
     {
         static void Main(string[] args)
         {
+            GeneratorTests tests = new GeneratorTests();
+
+            tests.Run100T();
         }
     }
 }
