26bc91e00f6f43d15cd51f7e92f47b0f52e641ef
Martin Milota <milotov@seznam.cz>
2016-11-27 18:18:57 +0100

    * Modified merge to do root consolidation

--- Utils/DataStructures/Heap/FibonacciHeap.cs
+++ Utils/DataStructures/Heap/FibonacciHeap.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using Utils.DataStructures.Internal;
 using Utils.DataStructures.Nodes;
 
@@ -28,6 +30,9 @@ namespace Utils.DataStructures
 
         internal HeapNode FirstRoot;
         internal HeapNode MinNode;
+
+        // We use a stack for this because it has sufficiently convenient ops and is handcrafted (a requirement)
+        private readonly Stack<HeapNode> _roots = new Stack<HeapNode>();
 
         private readonly NodeTraversalActions<TKey, TValue> _traversalActions;
 
@@ -169,24 +174,131 @@ namespace Utils.DataStructures
 
         public void Merge(FibonacciHeap<TKey, TValue> other)
         {
-            Count += other.Count;
-
-            if (Count == 0)
-            {
-                FirstRoot = other.FirstRoot;
-                MinNode = other.MinNode;
-                return;
-            }
-
-            FirstRoot.InsertBefore(other.FirstRoot); // Inserts the other roots after all of our roots
-
-            if (Comparer.Compare(other.MinNode.Key, MinNode.Key) < 0)
-                MinNode = other.MinNode;
+            Consolidate(other.FirstRoot, other.Count);
         }
 
         #endregion
 
         #region Helpers
+
+        [Flags]
+        private enum Bits
+        {
+            First = 1,
+            Add = 2,
+            Carry = 4,
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void Consolidate(HeapNode firstNode, int count, bool checkMin = false)
+        {
+            Debug.Assert(firstNode != null);
+
+            // Nodes should be ordered from the smallest -- this makes it the same as binary digit addition
+            HeapNode carry = null;
+            int currentOrder = 0;
+
+            // We end when we reach the end of the list and when there is no leftover carry
+            while (firstNode != null || carry != null)
+            {
+                // Set the order for the current iteration
+                if (carry != null)
+                    currentOrder++; // The carry propagates only to the next order
+                else if (firstNode.ChildrenCount > currentOrder)
+                    currentOrder = firstNode.ChildrenCount; // If there is no carry, we handle the next node in the list
+
+                // Assert root array size
+                if (_roots.Count == currentOrder)
+                    _roots.Push(null); // Forces resize; will be replaced by the joined node
+
+                // Setup the not-null flag -- inputs
+                HeapNode add = null;
+                Bits inputs = 0;
+
+                if (_roots[currentOrder] != null)
+                    inputs |= Bits.First;
+
+                if (firstNode != null && firstNode.ChildrenCount == currentOrder)
+                {
+                    // Set the Add node for this iteration -- it is only valid if it is of the current order
+                    add = firstNode;
+                    inputs |= Bits.Add;
+
+                    // Update firstNode for the next iteration; we work with add in this iteration
+                    firstNode = (HeapNode)firstNode.RightSibling;
+
+                    if (firstNode != add)
+                        firstNode.CutFromFamily(); // Remove it from the list
+                    else
+                        firstNode = null; // This is the only node left in the list -- set it to null to signal exit
+                }
+
+                if (carry != null)
+                    inputs |= Bits.Carry;
+
+                // Join the nodes together, update the roots and minNode and create the new carry
+                AddNodes(ref _roots.Buffer[currentOrder], add, ref carry, inputs, checkMin);
+            }
+
+            Count += count;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void AddNodes(ref HeapNode first, HeapNode add, ref HeapNode carry, Bits inputs, bool checkMin = false)
+        {
+            Debug.Assert(inputs > 0);
+
+            switch (inputs)
+            {
+                case Bits.First | Bits.Add:
+                case Bits.First | Bits.Add | Bits.Carry:
+                    var tmp = first;
+                    first = carry;
+                    carry = Join(tmp, add, checkMin);
+                    return;
+
+                case Bits.First | Bits.Carry:
+                    first = Join(first, carry, checkMin);
+                    carry = null;
+                    return;
+                case Bits.Add | Bits.Carry:
+                    first = Join(add, carry, checkMin);
+                    carry = null;
+                    return;
+
+                case Bits.First:
+                    return;
+                case Bits.Add:
+                    first = add;
+                    return;
+                case Bits.Carry:
+                    first = carry;
+                    carry = null;
+                    return;
+
+                default:
+                    throw new ArgumentOutOfRangeException("inputs", inputs, "This should not happen..");
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private HeapNode Join(HeapNode first, HeapNode add, bool checkMin = false)
+        {
+            int comp = Comparer.Compare(first.Key, add.Key);
+
+            HeapNode smaller = first;
+            HeapNode other = add;
+
+            if (comp > 0)
+                Swap(ref smaller, ref other);
+
+            smaller.AddChild(other);
+
+            if (checkMin && Comparer.Compare(smaller.Key, MinNode.Key) < 0)
+                MinNode = smaller;
+
+            return smaller;
+        }
 
         #endregion
     }
