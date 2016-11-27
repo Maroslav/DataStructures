8da7d36fe1b30a830ee495db7037e28c64d2dbe8
Martin Milota <milotov@seznam.cz>
2016-11-27 18:15:46 +0100

    * Made the internal buffer internal..

--- Utils/DataStructures/Misc/Stack.cs
+++ Utils/DataStructures/Misc/Stack.cs
@@ -9,12 +9,12 @@ namespace Utils.DataStructures
         private const float ReallocateFactor = 2f;
 
 
-        private T[] _buffer;
+        internal T[] Buffer;
         private int _head;
 
 
         public int Count { get { return _head; } }
-        public int Capacity { get { return _buffer.Length; } }
+        public int Capacity { get { return Buffer.Length; } }
 
         #endregion
 
@@ -25,7 +25,7 @@ namespace Utils.DataStructures
             if (initialCapacity <= 0)
                 throw new ArgumentOutOfRangeException("initialCapacity", "The capacity must not be negative.");
 
-            _buffer = new T[initialCapacity];
+            Buffer = new T[initialCapacity];
         }
 
         #endregion
@@ -34,14 +34,14 @@ namespace Utils.DataStructures
 
         public void Push(T item)
         {
-            _buffer[_head++] = item;
+            Buffer[_head++] = item;
             CheckReallocate();
         }
 
         public T Pop()
         {
             T res = Peek();
-            _buffer[--_head] = default(T);
+            Buffer[--_head] = default(T);
             return res;
         }
 
@@ -50,7 +50,7 @@ namespace Utils.DataStructures
             if (Count == 0)
                 throw new InvalidOperationException("The Stack<T> is empty.");
 
-            return _buffer[_head - 1];
+            return Buffer[_head - 1];
         }
 
         public T this[int idx]
@@ -59,14 +59,21 @@ namespace Utils.DataStructures
             {
                 if (idx < 0 || idx >= Count)
                     throw new ArgumentOutOfRangeException("idx", "The index was out of range of the array.");
-                return _buffer[idx];
+                return Buffer[idx];
+            }
+
+            internal set
+            {
+                if (idx < 0 || idx >= Count)
+                    throw new ArgumentOutOfRangeException("idx", "The index was out of range of the array.");
+                Buffer[idx] = value;
             }
         }
 
         public void Clear()
         {
             for (int i = 0; i < _head; i++)
-                _buffer[i] = default(T);
+                Buffer[i] = default(T);
 
             _head = 0;
         }
@@ -82,12 +89,12 @@ namespace Utils.DataStructures
 
             int newCapacity = (int)(Capacity * ReallocateFactor);
             T[] newBuffer = new T[newCapacity];
-            Array.Copy(_buffer, newBuffer, _head);
+            Array.Copy(Buffer, newBuffer, _head);
 
             for (int i = 0; i < _head; i++)
-                _buffer[i] = default(T);
+                Buffer[i] = default(T);
 
-            _buffer = newBuffer;
+            Buffer = newBuffer;
         }
 
         #endregion
