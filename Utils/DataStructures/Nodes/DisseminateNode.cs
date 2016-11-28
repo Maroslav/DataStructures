40f592c44742048a9a3c1a15bf19c2dee795425a
Martin Milota <milotov@seznam.cz>
2016-11-28 01:38:42 +0100

    * Added a preliminary version of Sift (copied from BinaryNode)

--- Utils/DataStructures/Nodes/DisseminateNode.cs
+++ Utils/DataStructures/Nodes/DisseminateNode.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Utils.DataStructures.Internal;
 
 namespace Utils.DataStructures.Nodes
 {
@@ -114,6 +115,98 @@ namespace Utils.DataStructures.Nodes
         #endregion
 
         #region Traversal
+
+        /// <summary>
+        /// Traverses the graph starting with each nodes children, followed by siblings.
+        /// </summary>
+        private bool Sift(NodeTraversalActions<TKey, TValue, DisseminateNode<TKey, TValue>, BinaryNodeAction> nodeActions)
+        {
+            // We have to use an iterative way because the default stack size of .net apps is 1MB
+            // and it's impractical to change it.....
+            var stack = nodeActions.TraversalStack;
+            Debug.Assert(stack.Count == 0);
+            stack.Push(GetNodeTraversalToken(this, BinaryNodeAction.Sift));
+
+            try
+            {
+                while (stack.Count > 0)
+                {
+                    var token = stack.Pop();
+
+                    switch (token.Action)
+                    {
+                        case BinaryNodeAction.Sift:
+                            if (!token.Node.HandleSift(stack, nodeActions))
+                                return false;
+                            break;
+
+                        case BinaryNodeAction.InAction:
+                            if (!nodeActions.InvokeInAction(token.Node))
+                                return false;
+                            break;
+
+                        case BinaryNodeAction.PostAction:
+                            if (!nodeActions.InvokePostAction(token.Node))
+                                return false;
+                            break;
+
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            finally
+            {
+                stack.Clear();
+            }
+
+            return true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool HandleSift(Stack<NodeTraversalToken<DisseminateNode<TKey, TValue>, BinaryNodeAction>> stack, NodeTraversalActions<TKey, TValue, DisseminateNode<TKey, TValue>, BinaryNodeAction> nodeActions)
+        {
+            // First and only visit to this node
+            if (!nodeActions.InvokePreAction(this))
+                return false;
+
+
+            // Push actions in reverse order
+            var right = RightSibling;
+            var left = LeftSibling;
+
+            if (right != null)
+            {
+                stack.Push(GetNodeTraversalToken(this, BinaryNodeAction.PostAction));
+                stack.Push(GetNodeTraversalToken(right, BinaryNodeAction.Sift));
+            }
+            else if (left != null)
+                // We need to store the action (it has to be executed after sifting through Left)
+                stack.Push(GetNodeTraversalToken(this, BinaryNodeAction.PostAction));
+
+            if (left != null)
+            {
+                stack.Push(GetNodeTraversalToken(this, BinaryNodeAction.InAction));
+                stack.Push(GetNodeTraversalToken(left, BinaryNodeAction.Sift));
+            }
+            else
+            {
+                // Handle missing children -- we can only invoke actions right away if children are null from left to right
+                if (!nodeActions.InvokeInAction(this))
+                    return false;
+
+                if (right == null)
+                    if (!nodeActions.InvokePostAction(this))
+                        return false;
+            }
+
+            return true;
+        }
+
+        private NodeTraversalToken<DisseminateNode<TKey, TValue>, BinaryNodeAction> GetNodeTraversalToken(DisseminateNode<TKey, TValue> node, BinaryNodeAction action)
+        {
+            return new NodeTraversalToken<DisseminateNode<TKey, TValue>, BinaryNodeAction>(node, action);
+        }
 
         #endregion
     }
