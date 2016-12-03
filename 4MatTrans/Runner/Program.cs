405915e5b0826833f11b6645cbd1dcfaeab11f66
Martin Milota <milotov@seznam.cz>
2016-12-03 18:58:11 +0100

    + Added the Matrix transpose solution + Added the cachesim program

---
+++ 4MatTrans/Runner/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using UtilsTests.FibHeap;
+
+namespace Runner
+{
+    class Program
+    {
+        private const string LogFolderName = "Log";
+
+        static void Main(string[] args)
+        {
+            var test = new FibGeneratorTests(LogFolderName);
+            test.BalancedTest();
+            test.Dispose();
+
+            test = new FibGeneratorTests(LogFolderName);
+            test.ImbalancedTest();
+            test.Dispose();
+
+            test = new FibGeneratorTests(LogFolderName);
+            test.MaliciousTest();
+            test.Dispose();
+        }
+    }
+}
